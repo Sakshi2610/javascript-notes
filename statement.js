5c708ac9a2672be52215672c831a5eccc604a9e0
Sakshi Rane <sakshiramchandrarane@gmail.com>
2024-02-08 20:11:49 +0530

    write about js statements

---
+++ statement.js
@@ -0,0 +1,22 @@
+/* A computer program is a list of "instructions" to be "executed" by a computer.
+
+In a programming language, these programming instructions are called statements.
+
+A JavaScript program is a list of programming statements.
+
+js statements includes 
+    values
+    operators
+    keywords
+    expressions
+    comments
+
+
+semicolons separate mutiple statements
+ let a,b,c;
+ a = 5;
+ b = 3;
+ c = a + b;
+
+ togetherly all the programs we write or will be written in future are called as "Statements".
+*/
--- variables.js
+++ variables.js
@@ -13,10 +13,13 @@ but when we assign the value in same name it overrides the  value and print the 
 --------------- to overcome this JavaScript introduces let ------------------------------------ */
 
 var age = 22
-
 if(age){
 console.log(age) // 22 coz it prints above age value i.e. 22
 var age = 20
 console.log(age);  // after again declaration it overrides the value i.e. 20
 }
 console.log(age) // and here also it will printed the overrided value i.e. 20
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
